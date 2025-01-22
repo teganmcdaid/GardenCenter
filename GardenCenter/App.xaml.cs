5fb1a1fe14b4f20c3503e73632d918b44c228057
Tegan Mc Daid <teganmcdaid@live.ie>
2025-01-22 18:55:39 +0000

    Add project files.

---
+++ GardenCenter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GardenCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GardenCenter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GardenCenter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
